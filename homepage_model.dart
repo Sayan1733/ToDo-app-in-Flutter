4fdcd84da3a8479e0e12d68398fea14978f3b4dc
Sayandip Duary <kekaduary@gmail.com>
2024-01-06 21:08:36 +0530

    homepage_model.dart

---
+++ homepage_model.dart
@@ -0,0 +1,37 @@
+import '/backend/backend.dart';
+import '/components/new_task_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'homepage_widget.dart' show HomepageWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class HomepageModel extends FlutterFlowModel<HomepageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for CheckboxListTile widget.
+
+  Map<ToDoRecord, bool> checkboxListTileValueMap = {};
+  List<ToDoRecord> get checkboxListTileCheckedItems =>
+      checkboxListTileValueMap.entries
+          .where((e) => e.value)
+          .map((e) => e.key)
+          .toList();
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
