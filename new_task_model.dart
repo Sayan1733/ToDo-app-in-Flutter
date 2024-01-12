282ae3945a36851e9a35e6466d7e168d66207406
Sayandip Duary <kekaduary@gmail.com>
2024-01-12 18:51:17 +0530

    Update new_task_model.dart

--- new_task_model.dart
+++ new_task_model.dart
@@ -1,29 +1,38 @@
 import '/backend/backend.dart';
+import '/component/empty/empty_widget.dart';
+import '/component/new_task/new_task_widget.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'new_task_widget.dart' show NewTaskWidget;
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'homepage_widget.dart' show HomepageWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
-class NewTaskModel extends FlutterFlowModel<NewTaskWidget> {
-  ///  State fields for stateful widgets in this component.
+class HomepageModel extends FlutterFlowModel<HomepageWidget> {
+  ///  State fields for stateful widgets in this page.
 
-  // State field(s) for TextField widget.
-  FocusNode? textFieldFocusNode;
-  TextEditingController? textController;
-  String? Function(BuildContext, String?)? textControllerValidator;
+  final unfocusNode = FocusNode();
+  // State field(s) for CheckboxListTile widget.
+
+  Map<ToDoRecord, bool> checkboxListTileValueMap = {};
+  List<ToDoRecord> get checkboxListTileCheckedItems =>
+      checkboxListTileValueMap.entries
+          .where((e) => e.value)
+          .map((e) => e.key)
+          .toList();
 
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
 
   void dispose() {
-    textFieldFocusNode?.dispose();
-    textController?.dispose();
+    unfocusNode.dispose();
   }
 
   /// Action blocks are added here.
