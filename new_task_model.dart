176a7be503465a809d1c35346e5230d91de95c9c
Sayandip Duary <kekaduary@gmail.com>
2024-01-12 18:52:15 +0530

    Update new_task_model.dart

--- new_task_model.dart
+++ new_task_model.dart
@@ -1,38 +1,36 @@
 import '/backend/backend.dart';
-import '/component/empty/empty_widget.dart';
-import '/component/new_task/new_task_widget.dart';
-import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'homepage_widget.dart' show HomepageWidget;
+import 'new_task_widget.dart' show NewTaskWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 
-class HomepageModel extends FlutterFlowModel<HomepageWidget> {
-  ///  State fields for stateful widgets in this page.
+class NewTaskModel extends FlutterFlowModel<NewTaskWidget> {
+  ///  State fields for stateful widgets in this component.
 
-  final unfocusNode = FocusNode();
-  // State field(s) for CheckboxListTile widget.
-
-  Map<ToDoRecord, bool> checkboxListTileValueMap = {};
-  List<ToDoRecord> get checkboxListTileCheckedItems =>
-      checkboxListTileValueMap.entries
-          .where((e) => e.value)
-          .map((e) => e.key)
-          .toList();
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
 
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
 
   void dispose() {
-    unfocusNode.dispose();
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
   }
 
   /// Action blocks are added here.
